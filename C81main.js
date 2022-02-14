0040d4e2a99a7cf130d450bdb65b1ca4111767fe
SahanaRajkumar <98228323+SahanaRajkumar@users.noreply.github.com>
2022-02-13 19:39:50 -0500

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,27 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+color="red";
+
+canvas.addEventListener("mousedown",my_mousedown);
+
+function my_mousedown(e){
+    mousex=e.cilentX-canvas.offsetLeft;
+    mousey=e.cilentY-canvas.offsetTop;
+    console.log("X= "+mousex+"y="+mousey);
+    circle(mousex,mousey);
+
+}
+
+
+function circle(mousex,mousey){
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=2;
+    ctx.arc(mousex,mousey,40,0.2*Math.PI);
+    ctx.stroke();
+
+
+
+
+
+}
